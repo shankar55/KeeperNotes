014949d0f2dcc7d79e2ab949859f550fe22f8c92
shankar55 <shankar511998@gmail.com>
2024-01-07 10:13:04 +0530

    Updates

---
+++ precache-manifest.1bdaeb9404eccde391d5693d7d0a0dd0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7ba6e32cba018d8a0ec81f2b723b81",
+    "url": "/KeeperNotes/index.html"
+  },
+  {
+    "revision": "e81fb2bb3ebaf4e03ecd",
+    "url": "/KeeperNotes/static/js/2.57aba40d.chunk.js"
+  },
+  {
+    "revision": "daf97f58e8f2570cb3c4",
+    "url": "/KeeperNotes/static/js/main.68465bbd.chunk.js"
+  },
+  {
+    "revision": "bf8833049b8cf4595b9d",
+    "url": "/KeeperNotes/static/js/runtime-main.c5b530da.js"
+  }
+]);
